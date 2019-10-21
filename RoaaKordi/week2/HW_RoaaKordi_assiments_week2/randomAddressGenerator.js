971edffd98efdb07e1c4e0a48cd0d6ba11250f19
roaa <roaak2009@gmail.com>
2019-10-21 15:30:54 +0300

    assignment 2  completed

--- RoaaKordi/week2/HW_RoaaKordi_assiments_week2/randomAddressGenerator.js
+++ RoaaKordi/week2/HW_RoaaKordi_assiments_week2/randomAddressGenerator.js
@@ -4,3 +4,33 @@ Your script should randomly select one item from each of these arrays and then u
 Each time you run the script, it should print a new randomly-generated address to the terminal. For example:
 node random-address.js
 => 34578 Dolphin Street, Wonka NY, 44506 */
+//made by Roaa Kordi
+//34578 Dolphin Street, Wonka NY, 44506
+let Address = ['street number', 'street name','state name' , 'zip code'];
+var streetNumber = [30, 250 ,591 , 700 , 677 ,337 , 1775];
+var streetName  = ['Memorial Drive', 'Brooksby Village ', 'Hartford Avenue', 'Worcester Rd', 'Massachusetts Ave', 'Fairhaven Commons','Parkhurst Rd' ];
+var stateName =['Framingham MA ',' Lynn MA  ','Hudson MA ','Leicester MA ','Halifax MA  ','Gardner MA ',' Danvers MA  '];
+var zipCode = [1440,2322,2019,1824,1020,2721,1701];
+
+
+//random Street Number
+var randomStNumber= Math.floor(Math.random()*streetNumber.length);
+var address1= streetNumber[randomStNumber];
+
+//random Street Name
+var randomStName= Math.floor(Math.random()*streetName.length);
+var address2= streetName[randomStName];
+
+//random State Name
+var randomState= Math.floor(Math.random()*stateName.length);
+var address3 = stateName[randomState];
+
+//random Zip code
+var randomZipCode = Math.floor(Math.random()*zipCode.length);
+var address4 = zipCode[randomZipCode];
+
+// Printing the result
+console.log('This is a Random Address in USA = '+ address1 + ' ' + address2 + ', ' + address3 + ', ' + address4 );
+
+
+
