156798c3fb54eba8e03a89cd4d7060d3fc4865cd
roaa <roaak2009@gmail.com>
2019-11-11 12:55:27 +0300

    add homework week 05

---
+++ RoaaKordi/week5/00-changing-the-channel/js/main.js
@@ -0,0 +1,73 @@
+// Show me what you've got!
+
+//SOME HINTS:
+// Return the value of an attribute:
+// $(selector).attr(attribute)
+
+/*
+function onClick(){
+   if( document.body.ul.li.getElementById() == '1'){
+    id="screen"
+   }
+}
+*/
+
+function channelone() {
+
+   document.getElementById("channel").src="img/deadpool.jpg";
+
+}
+
+function channeltwo() {
+
+   document.getElementById("channel").src="img/cnn.jpg";
+
+}
+function channelthree() {
+
+   document.getElementById("channel").src="img/blue-planet.jpg";
+   
+}
+function channelfour() {
+
+   document.getElementById("channel").src="img/good-will-hunting.jpg";
+   
+}
+function channelfive() {
+
+   document.getElementById("channel").src="img/gravity.jpg";
+   
+}
+function channelsix() {
+
+   document.getElementById("channel").src="img/koyaanisqatsi.jpg";
+}
+function channelseven() {
+
+   document.getElementById("channel").src="img/parts-unknown.jpg";
+   
+}
+function channeleight() {
+
+   document.getElementById("channel").src="img/star-wars-solo.jpeg";
+   
+}
+function channelnine() {
+
+   document.getElementById("channel").src="img/stranger-things.jpg";
+   
+}
+   
+
+
+   
+
+ 
+// Set the attribute and value:
+// $(selector).attr(attribute,value)
+
+// Set attribute and value using a function:
+// $(selector).attr(attribute,function(index,currentvalue))
+
+// Set multiple attributes and values:
+// $(selector).attr({attribute:value, attribute:value,...})
